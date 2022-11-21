6178200c48e00b8b37f5963048abd10eef4c84d3
hossein <hoss.yazdanfar@yahoo.com>
2022-11-21 17:57:33 +0330

    doc:

---
+++ Note.md
@@ -0,0 +1,7 @@
+#Notes
+I wanted to write tests, but I couldn't.
+
+Also, this was not my best, 
+
+usually I use Post method for most of requests and use DTOs for input and outputs
+
--- src/main/java/com/yazdanfar/assignment/controller/CsvController.java
+++ src/main/java/com/yazdanfar/assignment/controller/CsvController.java
@@ -13,8 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+
 
 @RestController
 public class CsvController {
